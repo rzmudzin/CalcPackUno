4f2e36203f832c22648a62f65bd55057ffa19584
robz <r@r.com>
2022-11-26 19:01:30 -0500

    Change class name, add new func

--- Sources/CalcPackUno/CalcPackUno.swift
+++ Sources/CalcPackUno/CalcPackUno.swift
@@ -5,9 +5,12 @@ public struct CalcPackUno {
     }
 }
 
-public class Calculator {
+public class CalculatorUno {
     public init() {}
     public func multiply(_ valueX: Int, with ValueY: Int) -> Int {
         valueX * ValueY
+    }
+    public func tenTimes(_ valueX: Int) -> Int {
+        valueX * 10
     }
 }
